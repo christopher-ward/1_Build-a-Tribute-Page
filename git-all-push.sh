cc7f7783a3a5e62a4ec455944817b23dfffd829e
chrisward90 <chrsward90@gmail.com>
2019-05-07 15:03:26 -0700

    Switch versions of the git push script[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Ded versions of the git push script

--- git-all-push.sh
+++ git-all-push.sh
@@ -1,20 +1,27 @@
-#!/bin/bash
+# !/bin/bash
 # Show status of git and prompt User for commit message to commit all the new changes to git and push to github
 # Also create conditionals to check for lack of message and also if User wishes to cancel commit after seeing status.
+echo ""
 
 git status
 
+echo ""
+echo "Enter 'n' to cancel"
+echo ""
 read -p 'commit -m ' varCommit
 
-#if [ -z "$varCommit" ]; then
-#	echo "Please put in a commit message."
-#	exit
-#elif [""$varCommit"" == "n"]; then
-#	echo "You canceled the script."
-#	exit
-#else
-#	echo ""$varCommit""
-#fi
+if [ -z "$varCommit" ]; then
+	echo ""
+	echo "Please put in a commit message."
+	exit
+elif [ "$varCommit" == "n" ] || [ "$varCommit" == "'n'" ]; then
+	echo ""
+	echo "You canceled the script."
+	exit
+else
+	echo 'Thanks!'
+	echo ""
+fi
 
 git add .
 
@@ -22,9 +29,14 @@ git commit -m "$varCommit"
 
 git push
 
+echo ""
 git status
+echo ""
 
-echo "It's done."
+echo "------------------------------Script Complete------------------------------"
+echo ""
+echo "commit -m: $varCommit"
+echo ""
+echo "Pushed to github"
 
-
-
+exit
